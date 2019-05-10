790dad1e0ea2ad5ad5ca3f9d8c69ddab3260a6da
lenovo <lenovo@DESKTOP-3EO33O0>
2019-05-10 08:32:42 +0530

    modification

---
+++ src/top/emp.java
@@ -0,0 +1,22 @@
+package top;
+
+public class emp {
+	
+
+	 private  int    empname;
+	 private  String   empsal;
+	public int getEmpname() {
+		return empname;
+	}
+	public void setEmpname(int empname) {
+		this.empname = empname;
+	}
+	public String getEmpsal() {
+		return empsal;
+	}
+	public void setEmpsal(String empsal) {
+		this.empsal = empsal;
+	}
+	 
+
+}
